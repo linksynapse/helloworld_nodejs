faf6379ebbbeeb41c91d01701c9711ee7164cc95
kailnet120_Eden <kailnet120@linksynapse.com>
2022-09-29 13:43:52 +0800

    error fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function base64DecodeUnicode(str) {
   // Convert Base64 encoded bytes to percent-encoding, and then get the original string.
-  percentEncodedStr = atob(str).split('').map(function(c) {
+  var percentEncodedStr = atob(str).split('').map(function(c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   }).join('');
 
